416296b0c177f4623b656aa1654e720ff13bf917
Vitaliy Horbatenko <vitalik_gor@yahoo.com>
2022-05-07 13:47:03 +0300

    edited proper period of ages echo command added

--- best_ever_players.sh
+++ best_ever_players.sh
@@ -5,3 +5,4 @@ while read years player1 player2 player3; do
 	printf "The period of time: %s\tnomination 1: %s\tnomination 2: %s\t nomination 3: %s \n" $years $player1 $player2 $player3
 done < best_ever_players	
 echo "Do you agree?"
+echo "What is your suggestion?"
